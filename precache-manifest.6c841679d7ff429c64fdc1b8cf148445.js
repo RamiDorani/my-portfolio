1249ab6a6b542122f0a534c1cc1e91422230bc0a
RAMI <ramid1703@gmail.com>
2020-10-22 15:27:33 +0300

    Updates

---
+++ precache-manifest.6c841679d7ff429c64fdc1b8cf148445.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c1b1f5a6f7b3ef0e1de1a60759ec92",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "db133e751d65cb04d83e",
+    "url": "/my-portfolio/static/css/main.c1ac7bfb.chunk.css"
+  },
+  {
+    "revision": "ef835370b21eaec67072",
+    "url": "/my-portfolio/static/js/2.9424ca87.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/my-portfolio/static/js/2.9424ca87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db133e751d65cb04d83e",
+    "url": "/my-portfolio/static/js/main.e080bb64.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "52497ef6e2a57915ff534c342e49a95b",
+    "url": "/my-portfolio/static/media/ComicNeue-Regular.52497ef6.ttf"
+  },
+  {
+    "revision": "27f813cadd3a3af891e01acf64272748",
+    "url": "/my-portfolio/static/media/Handlee-Regular.27f813ca.ttf"
+  },
+  {
+    "revision": "b25d6775cad1e807c8e9e0b5fc0777a0",
+    "url": "/my-portfolio/static/media/Minesweeper.b25d6775.png"
+  },
+  {
+    "revision": "9b94499ccea3bd82b24cb210733c4b5e",
+    "url": "/my-portfolio/static/media/Pacifico-Regular.9b94499c.ttf"
+  },
+  {
+    "revision": "eeb4dad66cf2ff3ee47b7ac7906913a0",
+    "url": "/my-portfolio/static/media/PatrickHand-Regular.eeb4dad6.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/my-portfolio/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "2a009057c5cb467151c34e381237338c",
+    "url": "/my-portfolio/static/media/angular.2a009057.svg"
+  },
+  {
+    "revision": "26f37ca60325a64210aa99d482b495f3",
+    "url": "/my-portfolio/static/media/c++.26f37ca6.svg"
+  },
+  {
+    "revision": "2c04ef7103810d2432cecd826e5ed8e1",
+    "url": "/my-portfolio/static/media/chef.2c04ef71.jpg"
+  },
+  {
+    "revision": "8fc622c3a2e2d992ec059cca61e3dfc0",
+    "url": "/my-portfolio/static/media/impact.8fc622c3.ttf"
+  },
+  {
+    "revision": "103d3087741f93742cc10cee7e303cbb",
+    "url": "/my-portfolio/static/media/me.103d3087.jpeg"
+  },
+  {
+    "revision": "0e48977ff49ac1b0e922f679422c20bf",
+    "url": "/my-portfolio/static/media/mongo.0e48977f.svg"
+  },
+  {
+    "revision": "04087cdedc0d7bd117ac1631f64a6604",
+    "url": "/my-portfolio/static/media/nodeJS.04087cde.svg"
+  },
+  {
+    "revision": "2d007b4014bc73619943e9ffc5d7c807",
+    "url": "/my-portfolio/static/media/programmer.2d007b40.png"
+  },
+  {
+    "revision": "3c36d42971246cab462f55ca81485d73",
+    "url": "/my-portfolio/static/media/react.3c36d429.svg"
+  },
+  {
+    "revision": "071283f7160154e367fb2bf07acc5930",
+    "url": "/my-portfolio/static/media/responsive-design.071283f7.png"
+  },
+  {
+    "revision": "99f5fb927dd8ab5009c52c1f8fa1270d",
+    "url": "/my-portfolio/static/media/vue.99f5fb92.svg"
+  }
+]);
