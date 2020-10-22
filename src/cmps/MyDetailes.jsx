bc29ea8031b45339c0f89b52cefa16260443067e
RAMI <ramid1703@gmail.com>
2020-10-22 17:49:34 +0300

    'update'

--- src/cmps/MyDetailes.jsx
+++ src/cmps/MyDetailes.jsx
@@ -4,19 +4,21 @@ import React, { Component } from 'react'
 export class MyDetailes extends Component {
     render() {
         return (
-            <div className="my-detailes-wrapper container" id="about">
-                <section className="my-detailes">
-                    <h2>About Me</h2>
-                    <p>Fullstack / Frontend Web Developer and I'm also a student for software engineering at shenkar collage and I am about to start my final year of my studies. I have knowledge and experience in writing end-2-end single-page-aplication using the LATEST web technologies such as React.js, Node.js, Redux and more.</p>
-                    <h2>Contact Me</h2>
-                    <p>
+                <section className="my-detailes-wrapper container" id="about">
+                    <div className="up">
+                    <h2 className="my-headers about-me-header">About Me</h2>
+                    <p className="p about-me-p">Fullstack / Frontend Web Developer and I'm also a student for software engineering at shenkar collage and I am about to start my final year of my studies. I have knowledge and experience in writing end-2-end single-page-aplication using the LATEST web technologies such as React.js, Node.js, Redux and more.</p>
+                    </div>
+                    <div className="down">
+                    <h2 className="my-headers contact-me-header">Contact Me</h2>
+                    <p className="p contact-me-p">
                     Rami veted <br />
                     Nahal Habesor 23, Tel-Aviv <br />
                     052-2029267 <br />
                     ramid1703@gmail.com <br />
                     </p>
+                    </div>
                 </section>
-            </div>
         )
     }
 }
