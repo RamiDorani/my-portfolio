325f09ae6ce6ca4a7a8114eebb4513ad8cea8492
RAMI <ramid1703@gmail.com>
2020-10-25 16:43:16 +0200

    'update'

--- src/cmps/Footer.jsx
+++ src/cmps/Footer.jsx
@@ -4,16 +4,16 @@ export  class Footer extends Component {
     render() {
         return (
             <div className="footer flex-col">
-               <section className="social-icons">
+               <section className="social-icons container">
                <a href="https://www.facebook.com/rami.dorani" target="_blank"><i className="fab fa-facebook-f"></i></a>
                <a href="https://www.linkedin.com/in/rami-veted/" target="_blank"><i className="fab fa-linkedin-in"></i></a>
                <a href="https://github.com/RamiDorani" target="_blank"><i className="fab fa-github"></i></a>
                <a href="https://wa.me/9720522029267" target="_blank"><i className="fab fa-whatsapp"></i></a>
                </section>
-               <section className="copy-rights">
+               <section className="copy-rights container">
                <p>&copy; 2020 | Rami Veted Web Developer</p>
                </section>
-               <section className="credits">
+               <section className="credits container">
                    Designed By <span>Rami Veted</span>
                </section>
             </div>
