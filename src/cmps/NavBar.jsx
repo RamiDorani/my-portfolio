252069094d4c13ec37add007096f83ac7be1dc81
RAMI <ramid1703@gmail.com>
2020-10-27 13:09:53 +0200

    'update'

--- src/cmps/NavBar.jsx
+++ src/cmps/NavBar.jsx
@@ -15,10 +15,10 @@ import React, { Component } from 'react'
                 <div className="screen" onClick={this.toggleMenu}></div>
                 <nav>
                     <ul className="main-nav clean-list flex">
-                    <li><a href="#">HOME</a></li>
-                    <li><a href="#about">ABOUT</a></li>
-                    <li><a href="#projects">PROJECTS</a></li>
-                    <li><a href="#contact">CONTACT</a></li>
+                    <li onClick={this.toggleMenu}><a href="#">HOME</a></li>
+                    <li onClick={this.toggleMenu}><a href="#about">ABOUT</a></li>
+                    <li onClick={this.toggleMenu}><a href="#projects">PROJECTS</a></li>
+                    <li onClick={this.toggleMenu}><a href="#contact">CONTACT</a></li>
                     </ul>
                     <span className="mobile-menu" onClick={this.toggleMenu}>☰</span>
                 </nav>
